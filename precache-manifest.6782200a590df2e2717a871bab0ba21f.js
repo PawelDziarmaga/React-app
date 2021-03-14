03a090f52d87d58fb3f4bb62a9a6d7f1ab27a45b
Pawel D <dziarmaga8@gmail.com>
2021-03-14 18:54:43 +0100

    Updates

---
+++ precache-manifest.6782200a590df2e2717a871bab0ba21f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8eda74a4758d644eff97",
+    "url": "/React-app/./static/css/main.ec02f68e.chunk.css"
+  },
+  {
+    "revision": "8eda74a4758d644eff97",
+    "url": "/React-app/./static/js/main.8eda74a4.chunk.js"
+  },
+  {
+    "revision": "54729f8275ccb1426bf0",
+    "url": "/React-app/./static/js/runtime~main.54729f82.js"
+  },
+  {
+    "revision": "e12795637c47cc4e6448",
+    "url": "/React-app/./static/js/2.e1279563.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/React-app/./static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/React-app/./static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/React-app/./static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "5fc5c91fe4edf4f01dcd2c31348dbb21",
+    "url": "/React-app/./index.html"
+  }
+];
